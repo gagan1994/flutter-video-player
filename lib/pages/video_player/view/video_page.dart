e7e9ecd0caa5bc4444972be84169ce44957b65eb
Gagan Patil <gaganpatil@IT-Admins-MacBook-Pro.local>
2021-02-23 10:28:01 +0530

    flicker video player

--- lib/pages/video_player/view/video_page.dart
+++ lib/pages/video_player/view/video_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_app_video_player/pages/video_player/widgets/anim_slider_widget.dart';
